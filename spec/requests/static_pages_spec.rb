51d73cbf97f943da20bfb9944f65b841ee2ca77a
Mu <julien.muresianu@gmail.com>
2013-02-09 12:46:14 +0100

    add title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,16 +4,21 @@ describe "Static Pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Human Writes'" do
+    it "should have the h1 'Human Writes'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_content('Human Writes')
+      page.should have_selector('h1', :text => 'Human Writes')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
         visit '/static_pages/home'
         page.should have_selector('title', 
-                                  :text => "Human Writes | Home")
+                                  :text => "Human Writes")
+    end
+
+    it "should NOT have a custom page title" do
+        visit '/static_pages/home'
+        page.should_not have_selector('title', :text => '| Home')
     end
   end
 
